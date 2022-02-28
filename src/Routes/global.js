ddd8650c7e3914545883c3ea9e5cf7ebb93f0359
asketsystem <asketsystem1@gmail.com>
2022-02-28 15:42:01 +0000

    Adding controller function

--- src/Routes/global.js
+++ src/Routes/global.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-// your routes paths and methods
+const globalControllers = {
+  healthyCheck(req, res) {
+    res.send("<h2>API is running</h2>");
+  },
+};
+module.exports = globalControllers;
+
+// routes paths and methods
 router.get("/api", (req, res) => {
   res.send("API online");
 });
