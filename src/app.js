9c927078700aefbc9a433b5d42cab102f5522ea6
asketsystem <asketsystem1@gmail.com>
2022-02-28 05:12:31 +0000

    Creating routes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 const express = require("express");
 const app = express();
+
+// server application
 app.listen(3000, () => console.log("Server is running at PORT 3000 🚀"));
+// routes
+app.get("/api", (req, res) => {
+  res.send("API online");
+});
